633b5c5979482270f8342ab4634cf9dd4734d4a5
Miles0gaming <miles0gaming@gmail.com>
2025-10-18 21:50:40 -0500

    Unfucked it

---
+++ overrides/shaderpacks/_0EuphoriaPatches Error Shader/shaders/errorTexts.glsl
@@ -0,0 +1,12 @@
+beginTextError(3, offset);
+    printString((_Y, _o, _u, _space, _n, _e, _e, _d, _space, _t, _o, _space, _h, _a, _v, _e, _space, _C, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _S, _h, _a, _d, _e, _r, _s, _under, _r, _5, _dot, _6, _dot, _1, _space, _i, _n, _s, _t, _a, _l, _l, _e, _d, _exclm));
+endText(textColor);
+
+offset.y += 1;
+
+beginTextError(3, offset);
+    printString((_P, _l, _e, _a, _s, _e, _space, _d, _o, _w, _n, _l, _o, _a, _d, _space, _i, _t, _space, _f, _r, _o, _m, _space, _h, _t, _t, _p, _s, _colon, _slash, _slash, _w, _w, _w, _dot, _c, _o, _m, _p, _l, _e, _m, _e, _n, _t, _a, _r, _y, _dot, _d, _e, _v, _slash, _space, _a, _n, _d, _space, _p, _l, _a, _c, _e, _space, _i, _t, _space, _i, _n, _t, _o, _space, _y, _o, _u, _r, _space, _s, _h, _a, _d, _e, _r, _p, _a, _c, _k, _s));
+    printLine();
+    printString((_f, _o, _l, _d, _e, _r, _dot));
+endText(textColor);
+
